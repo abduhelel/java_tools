626aad2999938e84b160d6bea6f979fbd4bc7fd6
‘abduhelel’ <timehelel@163.com>
2022-02-08 09:53:28 +0800

    file utils get files some dir

---
+++ java_tools/src/FileUtils.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * file utils
+ * @author Aaron
+ * @Date   2018-10-12
+ */
+public class FileUtils {
+
+    /**
+     * @Author： Aaron
+     * @Description：获取某个目录下所有直接下级文件，不包括目录下的子目录的下的文件，所以不用递归获取
+     * @Date：2018-10-12
+     */
+    public static List<String> getFiles(String path) {
+        List<String> files = new ArrayList<String>();
+        File file = new File(path);
+        File[] tempList = file.listFiles();
+
+        for (int i = 0; i < tempList.length; i++) {
+            if (tempList[i].isFile()) {
+                files.add(tempList[i].toString());
+                //文件名，不包含路径
+                //String fileName = tempList[i].getName();
+            }
+            if (tempList[i].isDirectory()) {
+                //这里就不递归了，
+            }
+        }
+        return files;
+    }
+}
